d4430de133e249f8004eb3a925841d01d2407b82
Rohan <rohan232215@gmail.com>
2021-03-30 20:08:12 +0530

    initial commit

---
+++ qna_platform/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+//import Body from './components/Body'
+import Signup from './components/Signup'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Body /> */}
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
